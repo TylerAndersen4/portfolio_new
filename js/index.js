00fb1785650139352f41c59d266d507d5b90cee3
Tyler Andersen <tylerandersen@Chases-MacBook-Pro.local>
2018-10-30 10:36:19 -0700

    add updates

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-setInterval(function() {
-  $("case1")
-      .velocity("transition.slideUpIn", 1250)
-      .delay(750)
-      .velocity({ opacity: 0 }, 750)
-}, 2000);
+// setInterval(function() {
+//   $("case1")
+//       .velocity("transition.slideUpIn", 1250)
+//       .delay(750)
+//       .velocity({ opacity: 0 }, 750)
+// }, 2000);
