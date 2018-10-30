cee9b7022f990e1b123855847833488baac4fd54
Tyler Andersen <tylerandersen@Chases-MacBook-Pro.local>
2018-10-30 10:28:09 -0700

    add v.2 updates

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
-$(document).ready(function() {
-  $("#root").load("./second_page.html")
-})
+setInterval(function() {
+  $("case1")
+      .velocity("transition.slideUpIn", 1250)
+      .delay(750)
+      .velocity({ opacity: 0 }, 750)
+}, 2000);
