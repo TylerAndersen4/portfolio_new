ecbd85bf8bbdc42306f39711a04b37e4d435ba62
Tyler Andersen <tylerandersen@Chases-MacBook-Pro.local>
2019-01-20 17:53:26 -0800

    make Kreation fixes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-// setInterval(function() {
-//   $("case1")
-//       .velocity("transition.slideUpIn", 1250)
-//       .delay(750)
-//       .velocity({ opacity: 0 }, 750)
-// }, 2000);
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+  offset: 100, // start scene after scrolling for 100px
+  duration: 400 // pin the element for 400px of scrolling
+  triggerElement: '#scroll-trigger' // point of execution
+})
+.setPin('#pinned-element1'); // the element we want to pin
+
+// Add Scene to ScrollMagic Controller
+controller.addScene(scene);
