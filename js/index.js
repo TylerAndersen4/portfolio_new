add02a08a987bdbe8b6153d0a666a06fadfcf4fb
Tyler Andersen <tylerandersen@Chases-MacBook-Pro.local>
2019-01-23 00:37:06 -0800

    update josh on homepage

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
-var controller = new ScrollMagic.Controller();
+$(function(){
+    $('.navbar').affix({
+      offset: {
+        /* Affix the navbar after scroll below header */
+        top: $("header").outerHeight(true)}
+    });
+});
 
-var scene = new ScrollMagic.Scene({
-  offset: 100, // start scene after scrolling for 100px
-  duration: 400 // pin the element for 400px of scrolling
-  triggerElement: '#scroll-trigger' // point of execution
-})
-.setPin('#pinned-element1'); // the element we want to pin
 
-// Add Scene to ScrollMagic Controller
-controller.addScene(scene);
+// var controller = new ScrollMagic.Controller();
+//
+// var scene = new ScrollMagic.Scene({
+//   offset: 100, // start scene after scrolling for 100px
+//   duration: 400 // pin the element for 400px of scrolling
+//   triggerElement: '#scroll-trigger' // point of execution
+// })
+// .setPin('#pinned-element1'); // the element we want to pin
+//
+// // Add Scene to ScrollMagic Controller
+// controller.addScene(scene);
